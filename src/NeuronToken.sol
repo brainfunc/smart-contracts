3ce24a0ade956c7ce61f94ac06b676c3599abf08
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-08-19 22:17:23 +0530

    Update with withdrawal function

--- src/NeuronToken.sol
+++ src/NeuronToken.sol
@@ -22,26 +22,35 @@ contract NeuronToken is ERC721BasicToken {
   function createNeuron(
     string birthTimeStamp, string category,
     string subCategory, string uri, address to_)
-    public payable onlyOwner {
+    public payable {
     // Making sure we are being paid apt amount
-    uint256 price = getPrice("category");
-    require(msg.value >= price);
+    // uint256 price = getPrice("category");
+    //require(msg.value >= price);
     // Adding collectible to contract set
     uint id = neurons.length;
     neurons.push(Neuron(birthTimeStamp, category, subCategory, uri));
     // Minting a crypto collectible
     _mint(to_, id);
+
     // Transferring excess back to sender
-    if (msg.value > price) {
-      uint256 priceExcess = msg.value - price;
-      msg.sender.transfer(priceExcess);
-    }
+    // if (msg.value > price) {
+    //   uint256 priceExcess = msg.value - price;
+    //   msg.sender.transfer(priceExcess);
+    // }
   }
 
   // Get Neuron uri for neuron of a particular id
-  function getJSONUriForNeuron(uint256 id)
+  function getNeuron(uint256 id)
   public view returns(string) {
     return neurons[id].uri;
+  }
+
+  function withdraw(uint256 amount) onlyOwner
+  public returns(bool){
+    address contractAddress = this;
+    require(amount <= contractAddress.balance);
+    owner.transfer(amount);
+    return true;
   }
 
  // Makes sure only owner has access
@@ -61,7 +70,7 @@ contract NeuronToken is ERC721BasicToken {
       keccak256(abi.encodePacked("brainstem"))) {
         return 0.02 ether;
       } else if(keccak256(abi.encodePacked(category)) ==
-      keccak256(abi.encodePacked("artery"))) {
+      keccak256(abi.encodePacked("arterial"))) {
         return 0.01 ether;
       }
       return 0.00 ether;
